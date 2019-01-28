fbc22b8c888df973374ceff53091aa2f4802568f
truongduychuc <truongduychuc138@gmail.com>
2019-01-28 14:13:31 +0700

    Add calculator.xml

--- js/myJsFunctions.js
+++ js/myJsFunctions.js
@@ -1,8 +1,23 @@
 function swap(a,b) {
     a+=b;
     b=a-b;
-    a-b;
+    a-=b;
 }
 function getRandomInteger(min,max) {
     return Math.floor(Math.random()*(max-min))+min;
+}
+function plus(a,b) {
+    return parseInt(a) + parseInt(b);  // if you write a+b when you return a value for a function, it will perform a plus between two strings.
+}
+function minus(a,b) {
+    return a-b;
+}
+function multiple(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    //If you don't type any number to input type number, it will return a zero number although it seems an empty string when you alert it.
+    if(b==0)
+        return "Can not divide for zero!"
+    else return a/b;
 }
